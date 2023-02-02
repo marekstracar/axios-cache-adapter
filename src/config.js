868ccc643081093e77c2df93192d70f255ae9221
marekstracar <53182419+marekstracar@users.noreply.github.com>
2023-02-02 15:33:33 +0100

    Http, xhr as default adapter

--- src/config.js
+++ src/config.js
@@ -30,7 +30,7 @@ const defaults = {
             const { method, url, ...rest } = requestData;
             const { baseURL } = config;
 
-            const api = Axios.create({...rest, baseURL, adapter: Axios.defaults.adapter});
+            const api = Axios.create({...rest, baseURL, adapter: ['http', 'xhr']});
 
             return api[method](url);
         }
